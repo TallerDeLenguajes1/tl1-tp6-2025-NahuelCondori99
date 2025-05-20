c054d021eeb4486da31f8c8130d53b7ea8f0ac9d
NahuelCondori99 <nahuelcondori62t@gmail.com>
2025-05-20 15:32:21 -0300

    Realice el ejercicio1 del TP

--- Program.cs
+++ Program.cs
@@ -6,3 +6,14 @@ a=10;
 b=a;
 Console.WriteLine("valor de a:"+a);
 Console.WriteLine("valor de b:"+b);
+
+string input = Console.ReadLine();
+int num = int.Parse(input);
+int invertido=0;
+Console.WriteLine("Valor del numero:"+num);
+while (num>0)
+{
+    invertido = invertido*10 + num%10;
+    num = num/10;
+}
+Console.WriteLine("Valor invertido:"+invertido);
